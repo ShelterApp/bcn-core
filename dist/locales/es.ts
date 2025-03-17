4153c38385df7ca166d0a307655256a86a0518fc
ShelterApp <61941223+ShelterApp@users.noreply.github.com>
2025-03-17 23:34:05 +0700

    Create es.ts

---
+++ dist/locales/es.ts
@@ -0,0 +1,445 @@
+/* tslint:disable */
+export default {
+    // Menu Left
+    ALL: "Todos",
+    MEN: "Hombres",
+    WOMEN: "Mujeres",
+    YOUTH_AND_KIDS: "NiÒos",
+    KIDS: "NiÒos",
+    SENIORS: "Ancianos",
+    DISABLED: "Discapac",
+    FAMILIES: "Familias",
+    LGBT: "Veterans",
+    VETERANS:"Veterans",
+    ANYONE: 'Todos',
+    // Authentication
+    SERVICE_PROVIDER_LOGIN: "Inicio de sesión de proveedor de servicios",
+    SERVICE_PROVIDER_SIGNUP: "Registro de proveedor de servicios",
+    DISPLAY_NAME_SIGNUP: "Nombre del servicio de la comunidad",
+    REQUIRED_INPUT_CTA: "Por favor introduzca {{value}}",
+    WELCOME_BACK: "Bienvenido de nuevo",
+    WELCOME_BACK_WITH_USER: "Bienvenido {{value}}.",
+    HOME: "Inicio",
+    FORGOT_PASSWORD: "Olvidé mi contraseña",
+    RESET_PASSWORD: "Restablecer contraseña",
+    OR_CREATE_AN_ACCOUNT: "O crear una cuenta",
+    SIGN_IN: "Inicio de sesión",
+    SIGN_UP: "Registrar",
+    CANCEL: "Cancela",
+    EMAIL: "Correo electrónico",
+    PASSWORD: "Contraseña",
+    LOGOUT: "Cerrar sesión",
+    LOGOUT_THIS_ACCOUNT: "Desconectarse de esta cuenta",
+    SIGN_IN_WITH: "Iniciar sesión con",
+    BY_CLICKING: "Estar de acuerdo con",
+    PRIVACY_POLICY: "Política de privacidad",
+    ACCEPT_TERM_OF_USE_AND_PRIVACY_POLICY:
+        "Acepto los términos de uso y la política de privacidad",
+    PLEASE_ACCEPT_TERMS: "Por favor acepte los términos para continuar",
+    SIGNED_OUT_SUCCESSFULLY: "Sesión cerrada con éxito",
+    PASSWORD_MIN_LENGTH:
+        "La contraseña debe tener al menos {{value}} caracteres.",
+    EMAIL_OR_PASSWORD_INVALID:
+        "Por favor revise su correo y contraseña, e inténtelo de nuevo",
+    OLD_PASSWORD_INVALID: "Por favor revise su contraseña e inténtelo de nuevo",
+    PASSWORD_NOT_MATCH: "Su nueva contraseña no coincide",
+    EMAIL_MUST_BE_A_VALID_EMAIL: "Por favor introduzca un correo nuevo",
+    REQUEST_RESET_PASSWORD_SUCCESSFULLY:
+        "Por favor revise su correo para restablecer su contraseña",
+    EMAIL_NOT_FOUND: "Correo no encontrado",
+    RESET_PASSWORD_SUCCESSFULLY: "Contraseña restablecida con éxito",
+    ROLE: "Role",
+    TOTAL_SERVICE: "Servicio total",
+    MINIMUM_TOTAL_SERVICE: "El servicio total mínimo fue {{value}}",
+    CREATED_AT: "Creado en",
+    LAST_LOGIN_METHOD: "Último método de inicio de sesión",
+    LAST_LOGIN_AT: "Último inicio de sesión a las",
+    LAST_UPDATED_AT: "Última actualización a las",
+    USER_NEED_NOT_SIGNUP:
+        "Los usuarios no necesitan registrarse. Esto es solo para proveedores de servicios.",
+
+    // Service
+    END_DATE_SHOULD_BE_GREATER:
+        "La fecha final debe ser igual o mayor que la fecha de inicio",
+    FLAG_AND_DONATE: "Marcar y donar",
+    IS_SHOW_CONTACT:
+        "Por favor póngase en contacto con este servicio para las horas",
+    IS_SHOW_FLAG: "Mostrar bandera",
+    IS_SHOW_DONATE: "Mostrar donar",
+    ADD_THIS_HEADER: "Añadir servicio al encabezado",
+    NEVER_EXPIRIES: "No finaliza",
+    OWNED_BY: "Propiedad de",
+    MANAGED_BY: "Gestionado por",
+    AVAILABLE_BEDS_LESS_THAN:
+        "Las camas disponibles deben ser las mismas o menos que las camas totales",
+    PLEASE_SELECT_AT_LEAST_ONE_SERVICE: "Seleccione por lo menos un servicio",
+    PLEASE_SELECT_AT_LEAST_ONE_CATEGORY:
+        "Seleccione por lo menos una categoría",
+    CHECK_TIME_START:
+        "La hora de cerrar debe ser mayor que la hora de apertura",
+    PLEASE_ENTER_SERVICE_TYPE:
+        "Por favor introduzca la descripción del servicio",
+    PLEASE_TYPE_TOTAL_BEDS: "Por favor introduzca el número total de camas",
+    PLEASE_ENTER_COUNTRY: "Por favor introduzca el país",
+    PLEASE_ADD_SCHEDULE: "Por favor añada el horario",
+    ADDED_SUCCESSFULLY: "Servicio añadido correctamente",
+    ADDED_SUCCESSFULLY_AND_YOU_WANT_TO_CREATE_NEW:
+        "Servicio añadido correctamente. Quiere añadir otro?",
+    UPDATE_SERVICE_SUCCESSFULLY: "Servicio modificado correctamente",
+    COMMUNITY_SERVICE_NAME: "Nombre del servicio de la comunidad",
+    ADDRESS: "Dirección",
+    ADDRESS_1: "Dirección 1",
+    ADDRESS_2: "Dirección 2",
+    ADD_TO_SCHEDULE: "Añadir a horario",
+    HOLIDAYS_CLOSED_DAYS: "Días cerrados por vacaciones",
+    CRITICAL_SERVICES: "Servicios críticos",
+    UPDATE_SERVICE: "Modificar servicio",
+    THIS_SERVICE_IS_NOT_APPROVED: "Este servicio está pendiente de aprobación",
+    THIS_SERVICE_IS_APPROVED:
+        "Este servicio está aprobado y visible para otros usuarios",
+    NO_SERVICES_ADDED: "No hay servicios añadidos",
+    ADDED_BY: "Añadido porí",
+    PHONE: "Teléfono",
+    WEBSITE: "Página web",
+    SERVICES: "Servicios",
+    SERVICE_TYPE: "Tipo de servicio",
+    CATEGORIES: "Categorías",
+    AGE_GROUP: "Grupo de edad",
+    SCHEDULE: "Horario",
+    CLOSED_DAYS: "Días cerrados",
+    COMMENT: "Comentario",
+    EDIT_SERVICE: "Editar servicio",
+    DELETE_SERVICE: "Eliminar servicio",
+    ADDED_TO_FAVORITE: "Añadido a favoritos",
+    REMOVED_TO_FAVORITE: "Eliminado de favoritos",
+    NAME: "Nombre",
+    SEARCH_SERVICE: "Buscar servicio",
+    NO_FAVORITES:
+        "No  hay servicios en favoritos. Puede añadir un servicio a favoritos haciendo click en la estrella en la esquina superior derecha de la página de detalles del servicio.",
+    NO_BEDS: "La información de la cama no está disponible en ningún servicio de refugio existente.",
+    CATEGORY: "Categoría",
+    CONTACT_SERVICE: "Contactar servicio",
+    NO_SERVICES_CLIENT:
+        "No se encontraron resultados para el criterio introducido {{value}}",
+    NO_MANAGE_SERVICE:
+        "No hay serivicios añadidos. Por favor haga click en añadir servicio en el menú lateral y después enlazar con",
+    NO_SHELTER: "No hay servicios dispuestos para la información de las camas",
+    ACCEPT: "Aceptar",
+    NO_DATA_SERVICES: "No se encontraron servicios",
+    DESC_LESS_THAN_VALUE: "Descripción en menos de {{value}} caracteres",
+    CONNECT_WITH_US: "Conectarse a este servicio?",
+    CLAIM_TITLE: "Reclama este servicio",
+    CLAIM_SERVICE:"Reclama este servicio",
+    REPORT_SERVICE_INFO:
+    'Informa información incorrecta',
+    LAST_UPDATE_ON: "Fecha de última modificación",
+    OPEN_24H: "Abierto las 24 horas",
+    TODAY_AT: "Hoy a las",
+    TOMORROW: "Mañana",
+    YESTERDAY: "Ayer",
+    LAST_WEEK: "La semana pasada",
+    CITY: "Ciudad",
+    STATE: "Estado",
+    ZIP: "Código postal",
+    COUNTRY: "País",
+    SERVICE_DETAILS: "Detalles del servicio",
+    CRITICAL_DESC: "Descripción de una oración",
+    SOCIAL_MEDIA_LINKS: "Enlaces de redes sociales",
+    SERVICE: "Servicio",
+    MINIMUM_TOTAL_BEDS: "El total mínimo de camas es {{value}}",
+    MINIMUM_AVAILABLE_BEDS: "El mínimo de camas disponibles es {{value}}",
+    EXPIRY_DATE: "Fecha de caducidad",
+    EXPIRY_TIME: "Tiempo de expiración",
+    MANAGED_USERS: "Usuarios administrados",
+    TOTAL_BEDS: "Camas totales",
+    START_DATE: "Fecha de inicio",
+    END_DATE: "Fecha final",
+    START_TIME: "Hora de inicio",
+    END_TIME: "Hora de finalización",
+    CONTACT_GROUP: "Grupo de contacto",
+    SCHEDULE_TYPE: "Tipo de horario",
+    SELECT_DAY: "Seleccionar día",
+
+    // Menu
+    OPTIONS: "Opciones",
+    OPEN_SERVICES: "Servicios abiertos",
+    TOP_KUDOS: "Mejores valoraciones",
+    MAP_VIEW: "Vista en el mapa",
+    CRISIS_LINES: "Líneas de emergencia",
+    HOMELESS_LIAISONS: "Enlaces sin hogar",
+    HOMELESS_LIAISON: "Enlaces sin hogar",
+    REGIONAL_COORDINATORS: "Coordinadores regionales",
+    MY_FAVORITES: "Favoritos",
+    ABOUT_ECYEH: "Sobre App",
+    SHOW_TUTORIAL: "Mostrar tutorial",
+    GIVE_FEEDBACK: "Dar opinión",
+    VOLUNTEER: "Voluntario",
+    DONATE: "Donar",
+
+    TERMS_OF_USE: "Términos de uso",
+    ADD_SERVICE: "Añadir servicio",
+    MANAGE_SERVICES: "Gestionar servicios",
+    MANAGE_FEEDBACK: "Gestionar opiniones",
+    UPDATE_SHELTER: "Modificar refugio",
+    MANAGE_PROFILE: "Gestionar perfil",
+    MANAGE_APPROVALS: "Gestionar aprobaciones",
+    MANAGE_PERMS: "Gestionar permisos",
+    MANAGE_USERS: "Gestionar usuarios",
+    MANAGE_SUPPER: "Gestionar super usuarios",
+    MANAGE_ACCESS: "Gestionar acceso de administrador",
+    CHANGE_PASSWORD: "Cambiar contraseña",
+    ENTER_NEW_PASSWORD: "Introduzca nueva contraseña",
+    LOGIN_AND_SIGNUP: "Inicio de sesión/registro de proveedor",
+
+    // Change Password
+    CONFIRM_PASSWORD: "Confirmar contraseña",
+    CURRENT_PASSWORD: "Contraseña actual",
+    NEW_PASSWORD: "Nueva contraseña",
+
+    // Update Profile
+    UPDATE_PROFILE: "Editar perfilí",
+    UPDATE_PROFILE_SUCCESSFULLY: "Perfil modificado con éxito",
+
+    // My Favorite
+    SEARCH_FAVORITES: "Buscar favoritos",
+    SEARCH_USERS: "Buscar usuarios",
+
+    // CRISLINES
+    SEARCH_LINES: "Buscar líneas de emergencia",
+    SEARCH_LIAISONS: "Buscar agente de enlace con la gente sin techo",
+    ADD_CRISIS_LINE: "Añadir línea de emergencia",
+    UPDATE_LINE: "Actualizar línea de emergencia",
+    UPDATE_CRISIS_LINE: "Actualizar línea de emergencia",
+    CRISIS_LINE_INFORMATION: "Información sobre línea de emergencia",
+    CRISIS_LINE_NAME: "Nombre de la línea de emergencia",
+    UPDATE_LINE_SUCCESSFULLY: "Línea de emergencia actualizada correctamente",
+    CHAT_WEB_LINK: "Link a web de chat",
+    AREA: "Area",
+    HOURS: "Horas",
+    DO_YOU_WANT_TO_DELETE_THIS: "Quieres borrar este {{value}}?í",
+    ADDED_SUCCESSFULLY_AND_YOU_WANT_TO_CREATE_NEW_CRISLINES:
+        " Línea de emergencia añadida con éxito, quiere añadir otra?",
+    YOU_REACHED_LIMIT_SERVICE:
+        "Ha alcanzado el límite máximo de servicios añadidos. Por favor contacte el administrador de App en ecyehappinfo@gmail.com para pedir permiso para añadir más servicios",
+    TEXT: "Texto",
+    CRISIS_LINE: "Línea de crisis",
+    EDIT_CRISIS_LINE: "Editar línea de crisis",
+    ADD_LINE_SUCCESSFULLY: "Línea de crisis agregado exitosamente.",
+    CRISIS_LINE_DESC: "Línea de crisis descripción",
+
+    // LIAISONS
+    LOCATION: "Localización",
+    COUNTY: "Condado",
+    POSITION: "Posición",
+    DISTRICT: "Distrito",
+    ADD_LIAISON: "Añadir enlace",
+    UPDATE_LIAISON: "Actualizar enlace",
+    LIAISON_NAME: "Nombre del enlace",
+    LIAISON_INFORMATION: "Información del enlace",
+    LIAISON_SUCCESSFULLY: "Enlace actualizado correctamente",
+    ADDED_SUCCESSFULLY_AND_YOU_WANT_TO_CREATE_NEW_LIAISON:
+        "Enlace añadido correctamente. Quiere añadir otro",
+    LIAISON: "Enlace",
+    SELECT_COUNTY: "Seleccionar condado",
+    LIAISON_TITLE: "Título de enlace",
+    EDIT_LIAISON: "Editar enlace",
+
+    // REGIONAL
+    ADD_REGIONS: "Añadir coordinador",
+    EDIT_REGIONS: "Edit región",
+    UPDATE_REGION: "Actualizar región",
+    UPDATE_REGION_SUCCESSFULLY: "Coordinador actualizado correctamente",
+    ADD_REGION_SUCCESSFUL: "Región añadir correctamente",
+    SELECT_REGION: "Seleccione región",
+    REGION: "Región",
+    REGIONS: "Regiones",
+    COUNTIES: "Condados",
+
+    // COORDINATOR
+    SEARCH_COORDINATOR: "Buscar coordinador",
+    UPDATE_COORDINATOR: "Actualizar Regional Coordinador",
+    EDIT_COORDINATOR: "Editar Regional Coordinador",
+    ADD_COORDINATOR: "Agregar Regional Coordinador",
+    ADD_ADDRESS: 'Añadir dirección',
+    MOBILE_PHONE: "Teléfono móvil",
+    FAX_PHONE: "Teléfono de fax",
+    OFFICE_PHONE: "Teléfono de oficina",
+    EXTENSION: "Extensión",
+    COORDINATOR: "Coordinador",
+    REGIONAL_COORDINATOR: "Regional Coordinador",
+
+    // #DBACKd
+    CONNECT_TO: "Conectarse a {{value}}?",
+    REPORT_TITLE: "Reportar información error o reclamar servicio",
+    SENT_FEEDBACK_SUCCESSFULLY: "Opinión enviada con éxito",
+    SENT_REPORT_SUCCESSFULLY: "Reporte enviado con éxito",
+    ARE_YOU_SURE_WANT_TO_ARCHIVE_THIS:
+        "Está seguro de que quiere archivar esto?",
+    FEEDBACK: "Opinóin",
+    SEND_FEEDBACK: "Enviar opinión",
+    AVAILABLE_BEDS: "Camas disponibles",
+    NO_FEEDBACKS:
+        "No tiene nuevas opiniones. Revise la ventana de Archivo para opiniones antiguas",
+    SEARCH_FEEDBACK: "Buscar opinión",
+    APP_FEEDBACK: "Opinión sobre app",
+    SERVICE_FEEDBACK: "Opinión sobre servicio",
+    ARCHIVE: "Archivo",
+    MY_FEEDBACKS: "Mis opiniones",
+    SUBJECT: "Sujeto",
+    SEND: "Enviar",
+
+    // LOCATION
+    CHOOSE_CITY_ZIP_FOR_SERVICES:
+        "Elija ciudad / código postal para los servicios",
+    CURRENT_LOCATION: "Ubicación actual",
+    SEARCH_CITY_NAME_OR_ZIP_CODE: "Buscar nombre de ciudad o código postal",
+    NO_MATCHING_RESULTS_FOUND:
+        "No se encontraron resultados coincidentes. Ingrese una ubicación diferente.",
+    CAN_NOT_GET_LOCATION:
+        "No se puede ubicar la ubicación actual. Vaya a Configuración -> Abrir ubicación",
+    ADD_YOUR_LOCATION: "Agrega tu ubicación",
+
+    // MANAGE APPROVALS
+    SEARCH_APPROVALS: "Aprobaciones de búsqueda",
+    APPROVE_SELECTED: "Aprobar seleccionado",
+    APPROVE_ALL: "Aprobar todo",
+    DELETE_SELECTED: "Eliminar seleccionado",
+    TUTORIALS: "Ecyeh Tutoriales",
+    APPROVED_SUCCESSFULLY: "Aprobado con éxito",
+
+    // USER
+    USER_PROFILE: "Perfil de usuario",
+    UPDATE_USER_SUCCESSFULLY: "Perfil de usuario actualizado con éxito",
+    SERVICE_PROVIDER: "Service Provider",
+
+    // PERMS
+    APPROVED_USER_AS_ADMIN_SUCCESSFULLY: "{{value}} añadido como Administrado",
+    REMOVED_USER_AS_ADMIN_SUCCESSFULLY:
+        "{{value}} eliminado como Administrador",
+
+    // DATE
+    SUNDAY: "Domingo",
+    MONDAY: "Lunes",
+    TUESDAY: "Martes",
+    WEDNESDAY: "Miércoles",
+    THURSDAY: "Jueves",
+    FRIDAY: "Viernes",
+    SATURDAY: "Sábado",
+
+    // SHOW TUTORIAL
+    I_AGREE: "Acepto",
+
+    NETWORK_REQUEST_FAILED:
+        "No se puede conectar al servidor. Por favor revise su conexión",
+
+    // Alert
+    YES: "Sí",
+    NO: "No",
+
+    // Chatbot
+    CHATBOT_FAQ: "Preguntas frecuentes sobre chatbot",
+
+    DESCRIPTION: "Descripción",
+    TITLE: "Título",
+    MESSAGE: "Mensaje",
+    PLEASE_INPUT_AT_LEAST_ONE_COORDINATOR:
+        "Por favor ingrese al menos un coordinador.",
+
+    ADD_LIAISON_SUCCESSFULLY: "Enlace agregado exitosamente",
+    UPDATE_LIAISON_SUCCESSFULLY: "Enlace actualizado con éxito",
+
+    ADD_COORDINATOR_SUCCESSFULLY: "Coordinador agregado exitosamente",
+    UPDATE_COORDINATOR_SUCCESSFULLY: "Coordinador actualizado con éxito",
+
+    ATTACHMENTS: "ARCHIVOS ADJUNTOS",
+
+    // Service detail
+    AVAILABLE_TOTAL_BEDS: "Disponibles/Total de camas",
+    USER: "Usuari",
+
+    DELETE_LIAISON_SUCCESSFUL: "¡Este enlace se eliminó con éxito!",
+    DELETE_REGION_SUCCESSFUL: "¡Esta región se eliminó correctamente!",
+    DELETE_SERVICE_SUCCESSFUL: "Este servicio se eliminó correctamente!",
+    DELETE_COORDINATOR_SUCCESSFUL:
+        "¡Este coordinador fue eliminado correctamente!",
+    DELETE_USER_SUCCESSFUL: "¡Este usuario fue eliminado correctamente!",
+    DELETE_LINE_SUCCESSFUL: "¡Esta línea de crisis se eliminó con éxito!",
+
+    // intro
+    SKIP: "Salta",
+    WELCOME_ECYEH: "Bienvenida a App",
+    MILES: 'Millas',
+    CLOSED_STILL: 'Cerrado hasta',
+    CLOSED_TILL: 'Cerrado hasta',
+    CLOSED_UNTIL: 'Cerrado hasta',
+    OPEN_STILL: 'Abierto hasta',
+    CLOSED_TILL_NEXT: 'Cerrado hasta el siguiente',
+    SLIDE_1:
+        "La pantalla de inicio de la aplicación muestra una lista de servicios con información como disponibilidad de camas en los refugios, horarios de apertura y cierre, distancia desde tu ubicación, etc. No dudes en dar Kudos a los servicios que te gusta utilizar.",
+    SLIDE_2_1:
+    "Puede hacer clic en cualquier servicio en la pantalla de inicio para obtener información detallada, incluidas las indicaciones para llegar a ese servicio en autobús desde su ubicación. Puede hacer clic en ",   
+    SLIDE_2_2:
+        " en la parte superior derecha de la pantalla de detalles para agregar ese servicio a Mis favoritos.",
+    SLIDE_3:
+        "Las pestañas en la parte inferior de la pantalla de inicio te ayudarán a navegar a recursos como Alimentos, Refugio, Salud, Recursos y Educación. Cada pestaña tiene subpestañas en la parte superior, como Ropa, Higiene, Transporte, etc., para ayudarte a limitar tu búsqueda.",
+    CLICK_ON: "Haciendo clic en",
+    SLIDE_4:
+        " en la parte superior de la pantalla de inicio, accede al menú de opciones, donde podrá filtrar los servicios según categorías como Jóvenes, Veteranos, Mujeres, etc. También puede filtrar los servicios que están abiertos ahora, cambiar a una vista de mapa, buscar líneas de crisis, ir a Mis favoritos, etc.",
+    SLIDE_5:
+        "Puede hacer clic en Líneas de crisis en el menú de opciones para comunicarse con las líneas directas nacionales para recibir apoyo emocional. También puede buscar líneas o servicios de crisis usando ",
+    SLIDE_6:
+    " en la pantalla de inicio de los servicios, podrá conectarse con el administrador de la aplicación o el proveedor de servicios para ese recurso.",
+    CLICKING: "Haciendo clic en ",
+    SLIDE_7_1:
+    "Puede ver los servicios en la vista de lista o en la vista de mapa, y ni siquiera puede buscar otra ciudad o código postal usando ",
+    SLIDE_7_2: " en la parte superior de la pantalla de inicio.",
+    SLIDE_8:
+        "para interactuar con nuestro chatbot impulsado por IA para encontrar servicios con facilidad.",
+    ACCEPT_TERMS: "ACEPTA LOS TÉRMINOS",
+    SLIDE_9: "Al hacer clic en Acepto, acepta los Términos de uso y la Política de privacidad de la aplicación Big Country Navigator., ",
+
+    FOOD: "Comida",
+    SHELTER: "Albergue",
+    HEALTH: "Salud",
+    RESOURCES: "Recursos",
+    EDUCATION: "Educación",
+    LEARNING:"Learning",
+    EMPLOYMENT: "Trabajos",
+    MEALS: "Comidas",
+    FOOD_PANTRIES: "Despensas de alimentos",
+    EMERGENCY: "Emergencia",
+    TRANSITIONAL: "Transicional",
+    MEDICAL: "Medical",
+    COUNSELING: "Asesoramiento",
+    CLOTHES: "Ropa",
+    HYGIENE: "Higiene",
+    TECH: "Technología",
+    ASSISTANCE: "Assistencia",
+
+    // placeholder
+
+    PH_COMMUNITY_SERVICE_NAME: "Non-Profit Name",
+    PH_LINE_NAME: "Ejemplo: Línea directa de violencia doméstica",
+    PH_TEXT_NUMBER: "Número de text",
+    PH_DISTRICT: "Ejemplo: Distrito Escolar de Allegheny Valley",
+    PH_LIAISON_TITLE: "Ejemplo: Supervisor",
+    PH_EXT: "Ejemplo: 12217",
+    PH_EMAIL: "Ejemplo: abc@gmail.com",
+    PH_DISPLAY_NAME_SIGNUP: "Ejemplo: Hotel California",
+    PH_REGION_NAME: "Ejemplo: por estado",
+    PH_COUNTIES: "Ejemplo: ______",
+    PH_ADDRESS_1: "Broadway & E Colfax Ave",
+    PH_ADDRESS_2: "Civic Center Park",
+    PH_CITY: "Denver",
+    PH_COUNTRY: "Seleccione país",
+    PH_SERVICE_DETAILS:
+        "Ejemplo: Despensa de alimentos, refugio de emergencia, prueba de VIH, ropa, entrenamiento de GED ...",
+    PH_TOTAL_BED: "1 o más",
+    PH_AVAILABLE_BEDS: "0 o más",
+    PH_AGE_GROUP: "Ejemplo: 18+, 17-21, 24 y menos, todos edades…",
+    PH_FULL_NAME: "Ejemplo: Nombre entero",
+    PH_COORDINATOR_NAME: "Ejemplo: Centro de Escuelas y Comunidades",
+
+    //Others
+    LANGUAGE: 'Language: English'
+};
